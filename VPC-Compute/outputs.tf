53bd5ceac864f1b46dadacd3fa68f9cfda631c24
Bharat Kakkad <bharat_jk@yahoo.com>
2026-05-25 19:03:05 -0400

    Amend files to support both AZ and two subnets

--- VPC-Compute/outputs.tf
+++ VPC-Compute/outputs.tf
@@ -7,13 +7,11 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  description = "List of public subnet IDs"
-  value       = [aws_subnet.public.id]
+  value = [aws_subnet.public1.id, aws_subnet.public2.id]
 }
 
 output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value       = [aws_subnet.private.id]
+  value = [aws_subnet.private1.id, aws_subnet.private2.id]
 }
 
 output "security_group_ssh_id" {
